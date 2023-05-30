c5dbeacf5b9d5927fdbe712c469349c00882728f
tuhinshuvra <tuhinshuvra18@gmail.com>
2023-05-30 11:56:52 +0600

    added cors update

--- index.js
+++ index.js
@@ -13,8 +13,12 @@ app.use(bodyParser.json({ limit: "5mb" }));
 app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
 
 // midddle wares
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
+
+app.use(cors({
+  origin: ['http://careersbangladesh.com', 'http://localhost:3000']
+}))
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v2wwlww.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
